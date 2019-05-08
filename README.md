d7f654a15ba0e08888be847f478d1497313a848b
Delon Newman <delnewman@salud.unm.edu>
2019-05-08 15:38:21 -0600

    ActiveRecord Adapter returns Relational::Adapter::ActiveRecordRelation instances

--- README.md
+++ README.md
@@ -11,16 +11,16 @@ Relational::ActiveRecord - An Relational adapter for ActiveRecord sources.
     end
 
     relational do
-      people = from('path/to/people.csv', schema: {created_at: :datetime, name: :string}).where(->(row) { person.created_at > Date.today - 1 })
-      users = from(User).select(:id, :name)
-      people.join(users).select(:id, :name, :created_at).rename(:id, :user_id).to('path/to/report.csv')
+      people = from('path/to/people.csv', schema: {created_at: :datetime, name: :string}).select(->(row) { person.created_at > Date.today - 1 })
+      users = from(User).project(:id, :name)
+      people.join(users).project(:id, :name, :created_at).rename(:id, :user_id).to('path/to/report.csv')
     end
     
 or
 
     Relational.from('path/to/people.csv')
               .join(Relational.from(User))
-              .select(:id, :name, :created_at)
+              .project(:id, :name, :created_at)
               .rename(:id, :user_id)
               .to('path/to/data-export.json')
 
--- lib/relational/adapter/activerecord.rb
+++ lib/relational/adapter/activerecord.rb
@@ -3,18 +3,38 @@ require 'activerecord'
 
 module Relational
   module Adapter
-    class ActiveRecord
+    class ActiveRecordRelation < Relation
       def dispatch?(data)
         data.is_a?(Class) and Set.new(data.ancestors).include?(ActiveRecord::Base) or data.is_a?(ActiveRecord::Relation)
       end
 
-      def self.from(data)
+      def self.from(data, opts = {})
+        meta = opts[:meta] || {}
         if data.is_a?(Class) and data.is_a?(ActiveRecord::Base)
-          Relational::Relation.new(data.attribute_names.map(&:to_sym), data.all)
+          new(data.attribute_names.map(&:to_sym), data.all, meta)
         elsif data.is_a?(ActiveRecord::Relation)
-          Relational::Relation.new(data.attribute_names.map(&:to_sym), data)
+          new(data.attribute_names.map(&:to_sym), data, meta)
         else
           raise Adapter::Error, "Don't know how to import data from #{data.inspect}"
+        end
+      end
+
+      attr_reader :ar_relation
+
+      def initialize(header, ar_relation, meta = {})
+        super(header, ar_relation, meta)
+        @ar_relation = ar_relation
+      end
+
+      def joins(*args)
+        self.from(@ar_relation.joins(*args), meta: meta)
+      end
+
+      def join(other)
+        if other.is_a?(Symbol) or other.is_a?(String) or other.is_a?(Hash)
+          joins(other)
+        else
+          super(other)
         end
       end
     end
